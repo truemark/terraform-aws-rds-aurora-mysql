88d3fabfe37e1bde84b6c3a4d7eaa78264d61753
Jim Williams <jwilliams@truemark.io>
2024-04-04 10:50:56 -0600

    Updating to latest version for RDS Aurora MySQL

--- README.md
+++ README.md
@@ -11,43 +11,57 @@ and the ability to create an optional RDS proxy.
 ## Example Usage
 ```
 module "db" {
-  source                 = "truemark/rds-aurora-mysql/aws"
-  version                = "0.0.4"
-  database_name          = "dbname"
-  deletion_protection    = true
-  engine_version         = "8.0.mysql_aurora.3.02.2"
-  family                 = "aurora-mysql8.0"
-  instance_class         = "db.t4g.large"
-  name                   = "dbname"
-  replica_count          = 0
-  subnets                = [ "subnet-0613436966e999", "subnet-0613436966ea998" ]
-  vpc_id                 = "vpc-0a6c8fae7776adb32"
+  source              = "truemark/rds-aurora-mysql/aws"
+  version             = "0.0.5"
+
+  auto_minor_version_upgrade      = false
+  ca_cert_identifier              = "rds-ca-rsa2048-g1"
+  db_parameters                   = local.config[data.aws_caller_identity.current.account_id]["db_parameters"]
+  db_subnet_group_name            = "mysqlcommon"
+  deletion_protection             = false
+  engine_version                  = "8.0.mysql_aurora.3.05.2"
+  instance_class                  = "db.t4g.large"
+  kms_key_id                      = join("", data.aws_kms_alias.db.*.target_key_arn)
+  manage_master_user_password     = true
+  master_username                 = "admin"
+  name                            = local.name
+  replica_count                   = local.environment == "prod" ? 1 : 0
+  skip_final_snapshot             = true
+  store_master_password_as_secret = false
+  subnets                         = local.subnets
+  tags                            = local.tags
+  vpc_id                          = local.vpc_id
 }
 ```
 ## Parameters
 The following parameters are supported:
 
 - apply_immediately
-- allowed_cidr_blocks
 - auto_minor_version_upgrade
 - backup_retention_period
 - ca_cert_identifier
 - cluster_tags
 - copy_tags_to_snapshot
-- create_cluster
+- create
 - create_proxy
 - create_security_group
+- create_db_subnet_group
 - database_name
-- db_parameters
 - db_parameter_group_name
 - db_parameter_group_tags
+- db_parameters
+- db_subnet_group_name
 - deletion_protection
+- egress_cidrs
 - engine_version
 - family
+- ingress_cidrs
 - instance_class
 - kms_key_id
+- manage_master_user_password
 - master_password
 - master_password_secret_name_prefix
+- master_user_secret_kms_key_id
 - master_username
 - name
 - password_secret_tags
@@ -60,10 +74,10 @@ The following parameters are supported:
 - proxy_idle_client_timeout
 - proxy_require_tls
 - proxy_secret_arns
-- replica_count
-- rds_cluster_parameters
 - rds_cluster_parameter_group_name
 - rds_cluster_parameter_group_tags
+- rds_cluster_parameters
+- replica_count
 - security_group_tags
 - share
 - share_tags
@@ -73,3 +87,4 @@ The following parameters are supported:
 - subnets
 - tags
 - vpc_id
+- vpc_security_group_ids
--- variables.tf
+++ variables.tf
@@ -1,7 +1,25 @@
-variable "tags" {
-  description = "A map of tags to add to all resources."
-  type        = map(string)
-  default     = {}
+variable "apply_immediately" {
+  description = "Determines whether or not any DB modifications are applied immediately, or during the maintenance window"
+  type        = bool
+  default     = true
+}
+
+variable "auto_minor_version_upgrade" {
+  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window. Default `false`"
+  type        = bool
+  default     = false
+}
+
+variable "backup_retention_period" {
+  description = "How long to keep backups for (in days)"
+  type        = number
+  default     = 7
+}
+
+variable "ca_cert_identifier" {
+  description = "The identifier of the CA certificate for the DB instance"
+  type        = string
+  default     = "rds-ca-rsa2048-g1"
 }
 
 variable "cluster_tags" {
@@ -15,34 +33,14 @@ variable "copy_tags_to_snapshot" {
   default     = false
 }
 
-variable "create_cluster" {
-  description = "True if the cluster should be created"
+variable "create" {
+  description = "Whether cluster should be created (affects nearly all resources)"
   default     = true
 }
 
-variable "master_password" {
-  description = "Password for the master user. If null, a random one is generated."
-  default     = null
-}
-
-variable "master_username" {
-  description = "Username for the master user."
-  default     = "root"
-}
-
-variable "store_master_password_as_secret" {
-  default = true
+variable "create_proxy" {
+  default = false
   type    = bool
-}
-
-variable "master_password_secret_name_prefix" {
-  default = null
-}
-
-variable "password_secret_tags" {
-  description = "Additional tags for the secrets"
-  type        = map(string)
-  default     = {}
 }
 
 variable "create_security_group" {
@@ -51,32 +49,26 @@ variable "create_security_group" {
   type        = bool
 }
 
-variable "security_group_tags" {
-  description = "Additional tags for the security group"
-  type        = map(string)
-  default     = {}
+variable "create_db_subnet_group" {
+  description = "Determines whether to create the database subnet group or use existing"
+  type        = bool
+  default     = true
 }
 
-variable "name" {}
-
-variable "vpc_id" {
-  description = "The ID of the VPC to provision into"
+variable "database_name" {
+  description = "Name for an automatically created database on cluster creation"
   type        = string
-}
-
-variable "subnets" {
-  description = "List of subnet IDs to use"
-  type        = list(string)
-}
-
-variable "family" {
-  description = "The database family"
-  default     = "aurora-mysql8.0"
+  default     = ""
 }
 
 variable "db_parameter_group_name" {
   description = "Optional aws_db_parameter_group name. Providing this will prevent the creation of the aws_db_parameter_group resource."
   default     = null
+}
+
+variable "db_parameter_group_tags" {
+  description = "A map of tags to add to the aws_db_parameter_group resource if one is created."
+  default     = {}
 }
 
 variable "db_parameters" {
@@ -89,35 +81,21 @@ variable "db_parameters" {
   default = []
 }
 
-variable "db_parameter_group_tags" {
-  description = "A map of tags to add to the aws_db_parameter_group resource if one is created."
-  default     = {}
-}
-
-variable "rds_cluster_parameter_group_name" {
-  description = "Optional aws_rds_cluster_parameter_group name. Providing this will prevent the creation of the aws_rds_cluster_parameter_group resource."
-  default     = null
-}
-
-variable "rds_cluster_parameters" {
-  description = "Map of the parameters to use in the aws_rds_cluster_parameter_group resource"
-  type = list(object({
-    name         = string
-    value        = string
-    apply_method = string
-  }))
-  default = []
-}
-
-variable "rds_cluster_parameter_group_tags" {
-  description = "A map of tags to add to the aws_rds_cluster_parameter_group resource if one is created."
-  default     = {}
-}
-
-variable "database_name" {
-  description = "Name for an automatically created database on cluster creation"
+variable "db_subnet_group_name" {
+  description = "The name of the subnet group name (existing or created)"
   type        = string
   default     = ""
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
+
+variable "egress_cidrs" {
+  description = "A list of CIDR blocks which are allowed to access the database"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
 
 variable "engine_version" {
@@ -126,111 +104,61 @@ variable "engine_version" {
   default     = "8.0.mysql_aurora.3.01.0" # max version supporting proxy
 }
 
+variable "family" {
+  description = "The database family"
+  default     = "aurora-mysql8.0"
+}
+
+variable "ingress_cidrs" {
+  description = "A list of CIDR blocks which are allowed to access the database"
+  type        = list(string)
+  default     = ["10.0.0.0/8"]
+}
+
 variable "instance_class" {
   description = "Instance type to use at master instance. If instance_type_replica is not set it will use the same type for replica instances"
   type        = string
   default     = "db.r6g.large"
 }
 
-variable "replica_count" {
-  description = "Number of read-only replicas to create."
-  type        = number
-  default     = 1
-}
-
-variable "apply_immediately" {
-  description = "Determines whether or not any DB modifications are applied immediately, or during the maintenance window"
-  type        = bool
-  default     = true
-}
-
-variable "skip_final_snapshot" {
-  description = "Should a final snapshot be created on cluster destroy"
-  type        = bool
-  default     = false
-}
-
-variable "allowed_cidr_blocks" {
-  description = "A list of CIDR blocks which are allowed to access the database"
-  type        = list(string)
-  default     = ["10.0.0.0/8"]
-}
-
-variable "backup_retention_period" {
-  description = "How long to keep backups for (in days)"
-  type        = number
-  default     = 7
-}
-
-variable "preferred_backup_window" {
-  description = "When to perform DB backups"
-  type        = string
-  default     = "02:00-03:00" # 7PM-8PM MST
-}
-
-variable "preferred_maintenance_window" {
-  description = "When to perform DB maintenance"
-  type        = string
-  default     = "sat:03:00-sat:05:00" # 8PM-10PM MST
-}
-
-variable "deletion_protection" {
-  type    = bool
-  default = false
-}
-
-variable "share" {
-  default = false
-  type    = bool
-}
-
-variable "share_tags" {
-  description = "Additional tags for the resource access manager share."
-  type        = map(string)
-  default     = {}
-}
-
-variable "create_proxy" {
-  default = false
-  type    = bool
-}
-
-variable "proxy_debug_logging" {
-  default = false
-  type    = bool
-}
-
-variable "proxy_idle_client_timeout" {
-  default = 1800
-  type    = number
-}
-
-variable "proxy_require_tls" {
-  default = true
-  type    = bool
-}
-
-variable "proxy_iam_auth" {
-  description = "One of REQUIRED or DISABLED"
-  default     = "REQUIRED"
-}
-
-variable "proxy_secret_arns" {
-  description = "List of AWS Secret ARNs containing credentials for use by the proxy."
-  type        = list(string)
-  default     = []
-}
-
-variable "snapshot_identifier" {
-  description = "DB snapshot to create this database from"
-  type        = string
-  default     = null
-}
-
 variable "kms_key_id" {
   description = "KMS Key used to encrypt RDS instance"
   type        = string
   default     = null
+}
+
+variable "manage_master_user_password" {
+  description = "Set to true to allow RDS to manage the master user password in Secrets Manager. Cannot be set if `master_password` is provided"
+  type        = bool
+  default     = true
+}
+
+variable "master_password" {
+  description = "Password for the master DB user. Note that this may show up in logs, and it will be stored in the state file. Required unless `manage_master_user_password` is set to `true` or unless `snapshot_identifier` or `replication_source_identifier` is provided or unless a `global_cluster_identifier` is provided when the cluster is the secondary cluster of a global database."
+  default     = null
+}
+
+variable "master_password_secret_name_prefix" {
+  default = null
+}
+
+variable "master_user_secret_kms_key_id" {
+  description = "The Amazon Web Services KMS key identifier is the key ARN, key ID, alias ARN, or alias name for the KMS key"
+  type        = string
+  default     = null
+}
+
+variable "master_username" {
+  description = "Username for the master user."
+  default     = "admin"
+}
+
+variable "name" {}
+
+variable "password_secret_tags" {
+  description = "Additional tags for the secrets"
+  type        = map(string)
+  default     = {}
 }
 
 variable "performance_insights_enabled" {
@@ -245,14 +173,122 @@ variable "performance_insights_retention_period" {
   default     = null
 }
 
-variable "auto_minor_version_upgrade" {
-  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window. Default `false`"
+variable "preferred_backup_window" {
+  description = "When to perform DB backups"
+  type        = string
+  default     = "02:00-03:00" # 7PM-8PM MST
+}
+
+variable "preferred_maintenance_window" {
+  description = "When to perform DB maintenance"
+  type        = string
+  default     = "sat:03:00-sat:05:00" # 8PM-10PM MST
+}
+
+variable "proxy_debug_logging" {
+  default = false
+  type    = bool
+}
+
+variable "proxy_iam_auth" {
+  description = "One of REQUIRED or DISABLED"
+  default     = "REQUIRED"
+}
+
+variable "proxy_idle_client_timeout" {
+  default = 1800
+  type    = number
+}
+
+variable "proxy_require_tls" {
+  default = true
+  type    = bool
+}
+
+variable "proxy_secret_arns" {
+  description = "List of AWS Secret ARNs containing credentials for use by the proxy."
+  type        = list(string)
+  default     = []
+}
+
+variable "rds_cluster_parameter_group_name" {
+  description = "Optional aws_rds_cluster_parameter_group name. Providing this will prevent the creation of the aws_rds_cluster_parameter_group resource."
+  default     = null
+}
+
+variable "rds_cluster_parameter_group_tags" {
+  description = "A map of tags to add to the aws_rds_cluster_parameter_group resource if one is created."
+  default     = {}
+}
+
+variable "rds_cluster_parameters" {
+  description = "Map of the parameters to use in the aws_rds_cluster_parameter_group resource"
+  type = list(object({
+    name         = string
+    value        = string
+    apply_method = string
+  }))
+  default = []
+}
+
+variable "replica_count" {
+  description = "Number of read-only replicas to create."
+  type        = number
+  default     = 1
+}
+
+variable "security_group_tags" {
+  description = "Additional tags for the security group"
+  type        = map(string)
+  default     = {}
+}
+
+variable "share" {
+  default = false
+  type    = bool
+}
+
+variable "share_tags" {
+  description = "Additional tags for the resource access manager share."
+  type        = map(string)
+  default     = {}
+}
+
+variable "skip_final_snapshot" {
+  description = "Should a final snapshot be created on cluster destroy"
   type        = bool
   default     = false
 }
 
-variable "ca_cert_identifier" {
-  description = "The identifier of the CA certificate for the DB instance"
+variable "snapshot_identifier" {
+  description = "DB snapshot to create this database from"
   type        = string
-  default     = "rds-ca-rsa2048-g1"
+  default     = null
+}
+
+variable "store_master_password_as_secret" {
+  description = "Set to true to allow self-management of the master user password in Secrets Manager"
+  default     = false
+}
+
+variable "subnets" {
+  description = "List of subnet IDs to use"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to provision into"
+  type        = string
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate to the cluster in addition to the security group created"
+  type        = list(string)
+  default     = []
 }
